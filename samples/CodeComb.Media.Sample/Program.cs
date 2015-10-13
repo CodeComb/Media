d0ce60aa9ffa77b798f59990d7efbc4a5a5483f4
あまみや ゆうこ <1@1234.sh>
2015-10-13 19:49:36 +0800

    init

---
+++ samples/CodeComb.Media.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeComb.Media.Sample
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var media = new Video("C:\\test\\test.flv");
+            media.Convert(".mp4", Quality.Smallest).SaveAs(@"C:\\test\\csh.mp4");
+        }
+    }
+}
